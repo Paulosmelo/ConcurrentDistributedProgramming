8fab86193827ace66cdb69172743dd4ccdc50962
Johnny-Herbert <jhmn@cin.ufpe.br>
2023-07-14 00:54:30 -0300

    Inicio da quarta entrega

---
+++ QuartaEntrega/TCP/dataReader.go
@@ -0,0 +1,42 @@
+package dataReaderTCP
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
+
+type DataReaderTCP struct{}
+
+func (DataReaderTCP) getData(quantLinhas int) [][]string {
+	var linhas = make([][]string, quantLinhas)
+	fmt.Println()
+	f, err := os.Open("data.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	csvReader := csv.NewReader(f)
+
+	for i := 0; i <= quantLinhas; i++ {
+		// read csv values using csv.Reader
+		data, err := csvReader.Read()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if i > 0 {
+			linhas[i-1] = append(linhas[i-1], data...)
+		}
+	}
+	f.Close()
+
+	return linhas[:]
+}
+
+// func main() {
+// 	linhas := getData(5)
+
+// 	fmt.Println(linhas)
+
+// }
---
+++ QuartaEntrega/TCP/server.go
@@ -0,0 +1,85 @@
+package dataReaderTCP
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+)
+
+func main() {
+	ServerTCP()
+}
+
+func ServerTCP() {
+	r, err := net.ResolveTCPAddr("tcp", "localhost:1313")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+
+	ln, err := net.ListenTCP("tcp", r)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+
+	fmt.Println("Server listening on:", r)
+
+	defer ln.Close()
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Printf(err.Error())
+			os.Exit(0)
+		}
+		go HandleTCPConnection(conn)
+	}
+}
+
+func HandleTCPConnection(conn net.Conn) {
+	//Close connection
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(0)
+		}
+	}(conn)
+
+	for {
+
+		buffer := make([]byte, 1024)
+		mLen, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("1")
+			fmt.Println(err)
+			os.Exit(0)
+		}
+
+		payloadAmount, err := strconv.Atoi(string(buffer[:mLen]))
+		if err != nil {
+			fmt.Println("2")
+			fmt.Println(err)
+			os.Exit(0)
+		}
+
+		r := dataReaderTCP.DataReaderTCP{}.getData(payloadAmount)
+
+		replyMsgBytes, err := json.Marshal(r)
+		if err != nil {
+			fmt.Println("3")
+			fmt.Println(err)
+			os.Exit(0)
+		}
+
+		_, err = conn.Write([]byte(replyMsgBytes))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(0)
+		}
+	}
+	// conn.Close()
+}
