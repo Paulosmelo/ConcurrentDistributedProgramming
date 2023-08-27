4990839a30a5638159b07ae2a03193e7f46bf4ce
Paulo Matheus <pmssm@cin.ufpe.br>
2023-08-27 20:16:07 -0300

    quinta entrega

---
+++ QuintaEntrega/RPC/client.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"os"
+	"strconv"
+	"time"
+	"log"	
+	"net/rpc"
+	"fmt"
+)
+
+func openLogFile(path string) (*os.File, error) {
+    logFile, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+    if err != nil {
+        return nil, err
+    }
+    return logFile, nil
+}
+
+func setUpLog(){
+	var test_n = ""
+
+	if(len(os.Args) > 2){
+		test_n = os.Args[2]
+	}
+
+	file, err := openLogFile("../tests/rpc_logs/results_rpc_"+test_n+".log")
+
+    if err != nil { log.Fatal(err)}
+	log.SetOutput(file)
+    log.SetFlags(0)
+}
+
+func client_RPC() {		
+	setUpLog()
+
+	var requestTime time.Duration
+
+	// conecta ao servidor
+	client, err := rpc.Dial("tcp", "localhost:1313")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// fecha conexão
+	defer client.Close()
+
+	
+	var reply [][]string
+
+	// Create request
+	request := 2
+
+	for i := 0; i < 10000; i++ {
+		// prepara request & start time
+		t1 := time.Now()
+
+		//fmt.Println(reply)
+
+		// invoca operação remota
+		client.Call("DataReader.GetDataRPC", request, &reply)
+				
+		requestTime = time.Now().Sub(t1)
+
+		if(len(os.Args) > 1 && os.Args[1] == "teste"){
+			log.Println(strconv.Itoa(int(requestTime.Nanoseconds())))
+		}
+	}
+}
+
+func main() {
+	go client_RPC()
+
+	_, _ = fmt.Scanln()
+}
